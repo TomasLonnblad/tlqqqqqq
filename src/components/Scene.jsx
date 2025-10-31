1361048789bd4cba2520593e7d593038ecc4cf53
Tomas <tomaslonnblad@gmail.com>
2025-10-31 19:46:42 +0100

    Update Scene.jsx

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -5,7 +5,7 @@ import { Environment, CameraControls } from '@react-three/drei'
 export default function Scene() {
 
     return (
-        <Canvas orthographic style={{background: "black"}} camera={{position: [2, 0, 1], zoom: 200}}>
+        <Canvas orthographic style={{background: "black"}} camera={{position: [2, 0, 1], zoom: 1000}}>
             <Model />
             <directionalLight intensity={3} position={[0, 0.1, 1]} />
             <Environment preset="city"/>
